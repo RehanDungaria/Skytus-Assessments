b02113734fbb765dfe00785f3d48ec9b2aff0cd3
RehanDungaria <rehandungaria@gmail.com>
2026-05-29 15:40:55 +0530

    Assessment-1

--- Assessment-1/portfolio/src/components/Hero.js
+++ Assessment-1/portfolio/src/components/Hero.js
@@ -1,10 +1,22 @@
 import React from "react";
+import profileImage from "../image/profile.jpg";
 
 const Hero = () => {
   return (
     <section id="home" className="hero">
-      <h1>Hello, I'm <span>Rehan Dungaria</span></h1>      <p>ReactJS Developer</p>
-      <button>Hire Me</button>
+      <div className="hero-container">
+
+        <div className="hero-text">
+          <h1>Hello, I'm Rehan</h1>
+          <p>ReactJS Developer | Web Designer</p>
+          <button>Hire Me</button>
+        </div>
+
+        <div className="hero-image">
+          <img src={profileImage} alt="Profile" />
+        </div>
+
+      </div>
     </section>
   );
 };
